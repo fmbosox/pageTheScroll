31a01d5ca682cd27b8defa1bd5dfa9f58ff695b8
Felipe Montoya <felipemontoya@akwa.local>
2017-10-04 19:20:31 -0700

    This is the first commint, learning scroll view yeah

--- pageTheScroll/ViewController.swift
+++ pageTheScroll/ViewController.swift
@@ -9,16 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    var images = [UIImageView] ()
+    var contentWidth:CGFloat = 0.0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // set the images in the array.
+        for index in 0...2 {
+            let image = UIImage(named: "icon\(index).png")
+            let imageView = UIImageView(image: image)
+            images.append(imageView)
+            
+            // set the new midPoint for use in scrolView
+            var newMidPoint: CGFloat = 0.0
+            newMidPoint = view.frame.midX + view.frame.width*CGFloat(index)
+            
+            contentWidth += newMidPoint
+            
+            // Add more views so scrol can scroll.
+            
+            scrollView.addSubview(imageView)
+            imageView.frame = CGRect(x: newMidPoint - 75, y: (view.frame.size.height/2)-75, width: 150, height: 150)
+            scrollView.contentSize = CGSize (width: contentWidth, height: view.frame.size.height)
+            
+        }
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
